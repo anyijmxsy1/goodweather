84932f8a310d11a17c4e8bbfb51fe5896afe55c2
jmxsy <jmxsy@21cn.com>
2018-08-01 01:59:14 +0800

    创建数据库

---
+++ app/src/main/java/com/geiliweike/administrator/goodweather/MyApplication.java
@@ -0,0 +1,25 @@
+package com.geiliweike.administrator.goodweather;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.litepal.LitePal;
+
+/**
+ * Created by Administrator on 2018/8/1.
+ */
+
+public class MyApplication extends Application {
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        context = getApplicationContext();
+        LitePal.initialize(context);
+    }
+
+    public static Context getContext(){
+        return context;
+    }
+}
+
